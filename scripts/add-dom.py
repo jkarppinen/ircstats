579c56883ad3b263cc26335aa1eb8d8351344b1c
Juhani Karppinen <juhani.k.karppinen@gmail.com>
2016-11-17 03:20:20 +0200

    Added Fork me on GitHub ribbon

--- scripts/add-dom.py
+++ scripts/add-dom.py
@@ -44,5 +44,14 @@ soup.head.append("""
 
 """)
 
+soup.body.append("""
+    <a href="https://github.com/jkarppinen/ircstats">
+    <img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/c6286ade715e9bea433b4705870de482a654f78a/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f77686974655f6666666666662e706e67" 
+    alt="Fork me on GitHub" 
+    data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_white_ffffff.png">
+    </a> 
+""")
+
+
 output_file.write(soup.prettify(formatter=None, encoding='utf-8'))
 output_file.close()

9e012040ee4aac86b86e022af7faddce993af729
Juhani Karppinen <juhani.k.karppinen@gmail.com>
2016-11-19 00:57:42 +0200

    Improved carousel.py variable handling

--- scripts/carousel.py
+++ scripts/carousel.py
@@ -11,10 +11,10 @@ import datetime
 
 def add_carousel_item(content, active=False):
 
-    html = '<div class="carousel-item">'
+    carousel_div = '<div class="carousel-item">'
     if active:
-        html = '<div class="carousel-item active">'
-    html = html + content + '</div>'
+        carousel_div = '<div class="carousel-item active">'
+    html = carousel_div + content + '</div></div>'
     return html
 
 conf_file = 'conf_ircstats.yaml'
@@ -29,26 +29,28 @@ output_file = open(vars['header'], 'r+')
 lines = output_file.readlines()
 output_file.seek(0)
 quote_html = """<div class="quote_container">
-               <div class="quote">""" + quote['quote'] + """</div>
-               <div class="quote_author"> - """ + quote['author'] + """</div>
-       </div>"""
+                   <div class="quote">%s</div>
+                   <div class="quote_author"> - %s</div>
+               </div>
+               """ % (quote['quote'], quote['author'])
+
 carousel_html = add_carousel_item(quote_html, True)
 
 xmas_diff = datetime.date(2016,12,24) - datetime.date.today()
 
 if (xmas_diff) >= datetime.timedelta(seconds=1):
-    quote_html = """<div class="quote_container">
+    quote_html = """
+        <div class="quote_container">
             <div class="quote">%s päivää jouluun!</div>
-            """
-    carousel_html = carousel_html + add_carousel_item(quote_html % str(xmas_diff.days), False).decode('utf-8').strip()
+        </div>""" % str(xmas_diff.days)
+
+    carousel_html = carousel_html + add_carousel_item(quote_html, False).decode('utf-8').strip()
 
 html = """
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            """ + carousel_html + """
-        </div>
-    </div>
-"""
+        <div class="carousel-inner">%s</div>
+    </div>""" % (carousel_html)
+
 output_file.write(html.encode('utf-8'))
 output_file.writelines(lines)
 output_file.close()
